d9201302a0e4782429cf91b8a08e93121801ca51
Davide Vanoni <davide95.v@gmail.com>
2023-02-17 16:07:41 +0800

    use mongodb as caching system too

--- exceptions/nubela_profile_not_found_exception.py
+++ exceptions/nubela_profile_not_found_exception.py
@@ -1,4 +1,4 @@
 class NubelaProfileNotFoundException(Exception):
     def __init__(self):
-        self.message = "Profile not found. This may occur when the profile does not exist or is private"
+        self.message = "Profile not found. This may occur when the profile does not exist, is private or you provided a company one"
         super().__init__(self.message)
